10ed51c47bf4b025729608a55bc5c8cfc71ed610
Talha Turk <talhaturkk@icloud.com>
2023-07-27 15:42:15 +0300

    taskshow componenti eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
 import TaskCreate from './components/TaskCreate'
 import TaskList from './components/TaskList'
+import { useState } from 'react'
 
 function App() {
-
+  const [tasks,setTasks] = useState([])
+  const createTask = (title, taskDesc) => {
+    const createdTasks = [
+      ...tasks,{
+        id: Math.round(Math.random()*999999),
+        title: title,
+        taskDesc: taskDesc
+      }
+    ];
+    setTasks(createdTasks);
+  }
 
   return (
     <div className='App'>
-      <TaskCreate />
+      <TaskCreate onCreate={createTask}/>
       <h1>Görevler</h1>
-      <TaskList />
+      <TaskList tasks={tasks}/>
     </div>
   )
 }
