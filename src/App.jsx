081bdcf43a8dea592d32aa69cf4326b30dec6915
Talha Turk <talhaturkk@icloud.com>
2023-07-27 13:44:16 +0300

    task-create olusturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import TaskCreate from './components/TaskCreate'
+import TaskList from './components/TaskList'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <TaskCreate />
+      <h1>Görevler</h1>
+      <TaskList />
+    </div>
   )
 }
 
